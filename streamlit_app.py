9e1667c78a9d319cac0a78e4a7acf86863b462e4
Amar <104140871+realdumbslayer@users.noreply.github.com>
2024-02-08 13:18:11 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,7 @@ import streamlit as st
 st.title('My First Streamlit App')
 with st.chat_message("user"):
  st.write("Hello!")
+ 
+with st.chat_message("assistant"):
+    st.write("Hello human")
+    st.bar_chart(np.random.randn(30, 3))
