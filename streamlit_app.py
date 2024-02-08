4ed53d53be2590a46c280ea282deb50952690f00
Amar <104140871+realdumbslayer@users.noreply.github.com>
2024-02-08 14:09:48 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,42 +2,47 @@ import streamlit as st
 import random
 import time
 
+from openai import OpenAI
+import streamlit as st
+from langchain.llms import HuggingFaceHub
+from langchain.document_loaders import PyPDFLoader,PyPDFDirectoryLoader
+from langchain.text_splitter import CharacterTextSplitter,RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceInferenceAPIEmbeddings
+from langchain.vectorstores import  Chroma
+from langchain_community.vectorstores import Chroma
+from langchain.chains import RetrievalQA, LLMChain
+import os
 
-# Streamed response emulator
-def response_generator():
-    response = random.choice(
-        [
-            "Hello there! How can I assist you today?",
-            "Hi, human! Is there anything I can help you with?",
-            "Do you need help?",
-        ]
-    )
-    for word in response.split():
-        yield word + " "
-        time.sleep(0.05)
+st.title("ChatGPT-like clone")
 
 
-st.title("Simple chat")
+client= HuggingFaceHub(hf_key=st.secrets["HUGGINGFACEHUB_API_TOKEN"])
+HF_TOKEN= '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'
+#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
 
-# Initialize chat history
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-# Display chat messages from history on app rerun
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-# Accept user input
 if prompt := st.chat_input("What is up?"):
-    # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
-    # Display user message in chat message container
     with st.chat_message("user"):
         st.markdown(prompt)
 
-    # Display assistant response in chat message container
     with st.chat_message("assistant"):
-        response = st.write_stream(response_generator())
-    # Add assistant response to chat history
+        stream = client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        )
+        response = st.write_stream(stream)
     st.session_state.messages.append({"role": "assistant", "content": response})
