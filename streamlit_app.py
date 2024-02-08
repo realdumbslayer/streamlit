83f72cdfac45ceab591786e0c9a92d53ab673fed
Amar <104140871+realdumbslayer@users.noreply.github.com>
2024-02-08 11:38:46 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,60 +4,99 @@ from streamlit_extras.colored_header import colored_header
 from streamlit_extras.add_vertical_space import add_vertical_space
 from hugchat import hugchat
 
+!pip install -q langchain
+!pip install -q sentence_transformers
+!pip install -q torch
+!pip install -q sentencepiece
+!pip install -q transformers
+!pip install -q accelerate
+!pip install -q pypdf
+!pip install -q tiktoken
+!pip install -q streamlit
+!pip install -q chromadb
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader, PyPDFDirectoryLoader
+from langchain.chains.summarize import load_summarize_chain
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+from transformers import pipeline
+import torch
+import base64
+
+#model and tokenizer loading
+checkpoint = "MBZUAI/LaMini-Flan-T5-248M"
+tokenizer = T5Tokenizer.from_pretrained(checkpoint)
+base_model = T5ForConditionalGeneration.from_pretrained(checkpoint, device_map='auto', torch_dtype=torch.float32)
+
+#file loader and preprocessing
+def file_preprocessing(file):
+    !mkdir pdfs
+    ! gdown 1HtjVe14jeSXMAle84K8Q3xxXtdXSnKrh -O pdfs/CENG646_Lecture_5_part_1.pdf
+    ! gdown 1e6u-XehOPzcuBtQNOyR66mUh-m7mqObA -O pdfs/CENG646_Lecture_5_part_2.pdf
+    ! gdown 1egTaXJXl3GTmdKxVkYq1qSnzcA8JYq-R -O pdfs/Lecture_Clustering.pdf
+
+
+    loader = PyPDFDirectoryLoader("pdfs")
+    pages = loader.load_and_split()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=50)
+    texts = text_splitter.split_documents(pages)
+    final_texts = ""
+    for text in texts:
+        print(text)
+        final_texts = final_texts + text.page_content
+    return final_texts
+
+#LLM pipeline
+def llm_pipeline(filepath):
+    pipe_sum = pipeline(
+        'summarization',
+        model = base_model,
+        tokenizer = tokenizer,
+        max_length = 500,
+        min_length = 50)
+    input_text = file_preprocessing(filepath)
+    result = pipe_sum(input_text)
+    result = result[0]['summary_text']
+    return result
+
+@st.cache_data
+#function to display the PDF of a given file
+def displayPDF(file):
+    # Opening file from file path
+    with open(file, "rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+
+    # Embedding PDF in HTML
+    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="600" type="application/pdf"></iframe>'
+
+    # Displaying File
+    st.markdown(pdf_display, unsafe_allow_html=True)
+
+#streamlit code
+st.set_page_config(layout="wide")
+
+def main():
+    st.title("Document Summarization App")
+
+    uploaded_file = st.file_uploader("Upload your PDF file", type=['pdf'])
+
+    if uploaded_file is not None:
+        if st.button("Summarize"):
+            col1, col2 = st.columns(2)
+            filepath = "data/"+uploaded_file.name
+            with open(filepath, "wb") as temp_file:
+                temp_file.write(uploaded_file.read())
+            with col1:
+                st.info("Uploaded File")
+                pdf_view = displayPDF(filepath)
+
+            with col2:
+                summary = llm_pipeline(filepath)
+                st.info("Summarization Complete")
+                st.success(summary)
+
+
+
+if __name__ == "__main__":
+    main()
 st.set_page_config(page_title="HugChat - An LLM-powered Streamlit app")
-
-# Sidebar contents
-with st.sidebar:
-    st.title('🤗💬 HugChat App')
-    st.markdown('''
-    ## About
-    This app is an LLM-powered chatbot built using:
-    - [Streamlit](https://streamlit.io/)
-    - [HugChat](https://github.com/Soulter/hugging-chat-api)
-    - [OpenAssistant/oasst-sft-6-llama-30b-xor](https://huggingface.co/OpenAssistant/oasst-sft-6-llama-30b-xor) LLM model
-    
-    💡 Note: No API key required!
-    ''')
-    # add_vertical_space(5)
-    # st.write('Made with ❤️ by [Data Professor](https://youtube.com/dataprofessor)')
-
-# Generate empty lists for generated and past.
-## generated stores AI generated responses
-if 'generated' not in st.session_state:
-    st.session_state['generated'] = ["I'm HugChat, How may I help you?"]
-## past stores User's questions
-if 'past' not in st.session_state:
-    st.session_state['past'] = ['Hi!']
-
-# Layout of input/response containers
-input_container = st.container()
-colored_header(label='', description='', color_name='blue-30')
-response_container = st.container()
-
-# User input
-## Function for taking user provided prompt as input
-def get_text():
-    input_text = st.text_input("You: ", "", key="input")
-    return input_text
-## Applying the user input box
-with input_container:
-    user_input = get_text()
-
-# Response output
-## Function for taking user prompt as input followed by producing AI generated responses
-def generate_response(prompt):
-    chatbot = hugchat.ChatBot()
-    response = chatbot.chat(prompt)
-    return response
-
-## Conditional display of AI generated responses as a function of user provided prompts
-with response_container:
-    if user_input:
-        response = generate_response(user_input)
-        st.session_state.past.append(user_input)
-        st.session_state.generated.append(response)
-        
-    if st.session_state['generated']:
-        for i in range(len(st.session_state['generated'])):
-            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
-            message(st.session_state["generated"][i], key=str(i))
